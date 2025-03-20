a797df3362d06438147151bd5b43dbf6f2df5966
Mireilla <2981402L@student.gla.ac.uk>
2025-03-20 14:55:16 +0000

    no.14 commit

--- pages/views.py
+++ pages/views.py
@@ -93,7 +93,6 @@ def user_login(request):
           if form.is_valid():
                username = form.cleaned_data.get('username')
                password = form.cleaned_data.get('password')
-               print(username)
                user = authenticate(request, username=username, password=password)
                if user is not None:
                     login(request, user)
